3ceceac0f8acb464b586ae9987da8443dac2d0c3
Yuki-Kanamori <y.kanamory@gmail.com>
2021-02-24 14:21:55 +0900

    revised

--- summary_survey.R
+++ summary_survey.R
@@ -12,9 +12,7 @@ dir_output = "/Users/Yuki/Dropbox/業務/若鷹丸調査結果まとめ_東北�
 pointsdata = read.xlsx(paste0(dir_input, "/in_操業記録データ.xlsx"), sheet = 1, startRow = 1)
 pointsdata = pointsdata[, c(1, 16:20)]
 colnames(pointsdata) = c("station", "lat1", "lat2", "lon1", "lon2", "memo")
-pointsdata = pointsdata %>% mutate(lat = lat1+lat2/60, lon = lon1+lon2/60, date = str_sub(memo, -5, -2)) %>% select(station, lon, lat, memo, date)
-# unique(pointsdata$station)
-# pointsdata$station = factor(pointsdata$station, levels = c("A", "B", "C", "D", "E", "F", "G", "H"))
+pointsdata = pointsdata %>% mutate(lat = lat1+lat2/60, lon = lon1+lon2/60, date = str_sub(memo, -5, -2), station2 = ifelse(is.na(as.numeric(str_sub(memo, 2, 3))), str_sub(memo, 1, 2), str_sub(memo, 1, 1))) %>% filter(str_length(station2) == 1) %>% select(station, lon, lat, memo, date)
 
 
 
