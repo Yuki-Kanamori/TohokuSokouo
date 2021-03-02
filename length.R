44d5a4c4ff10d86c60be2dc8af6259d3c13e3333
Yuki-Kanamori <y.kanamory@gmail.com>
2021-03-02 12:31:05 +0900

    small changes

--- length.R
+++ length.R
@@ -46,35 +46,42 @@ plot_length = function(data){
   
   for(i in 1:length(sp)){
     # data2 = data %>% dplyr::filter(sp == sp[i]) #なぜかうまく引っ掛からない
-    i = 15
+    i = 5
     data2 = data[str_detect(data$sp, sp[i]), ]
     check = data2 %>% group_by(year) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
     remove = check$year
     
-    check2 = data2 %>% group_by(size_cate) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
-    remove2 = as.numeric(check2$size_cate)
-    remove2 = remove2[-1]
+    # check2 = data2 %>% group_by(size_cate) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
+    # remove2 = as.numeric(check2$size_cate)
+    # remove2 = remove2[-1]
     
     summary(data2)
     
+    # if(nrow(check) > 0){
+    #   if(nrow(check2) > 1){
+    #     data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove, size_cate < min(remove2))
+    #   }else{
+    #     data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove) 
+    #   }
+    # }else{
+    #   if(nrow(check2) > 1){
+    #     data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(nsize < min(remove2))
+    #   }else{
+    #     data3 = data2
+    #   }
+    # }
+    
     if(nrow(check) > 0){
-      if(nrow(check2) > 1){
-        data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove, size_cate < min(remove2))
-      }else{
-        data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove) 
-      }
+      data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove)
     }else{
-      if(nrow(check2) > 1){
-        data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(nsize < min(remove2))
-      }else{
-        data3 = data2
-      }
+      data3 = data2
     }
     
-    g = ggplot(data3, aes(x = nsize, y = B/1000, fill = NS))
+    
+    g = ggplot(data3, aes(x = as.numeric(size_cate), y = B/1000, fill = NS))
     b = geom_bar(position="dodge", stat = "identity", width = 0.8, colour = "black")
     f = facet_wrap(~ year, ncol = 6)
-    c = scale_fill_manual(values = c("black", NA))
+    c = scale_fill_manual(values = c("black", "red"))
     lab = labs(x = "体長（cm）", y = "尾数 (百万尾)", colour = "エリア")
     
     th = theme(panel.grid.major = element_blank(),
@@ -89,9 +96,11 @@ plot_length = function(data){
                #legend.position = c(0.75, 0.05),
                legend.background = element_rect(fill = "white", size = 0.4, linetype = "solid", colour = "black"))
     
-    fig = g+b+f+c+lab+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(expand = c(0, 0), breaks = seq(0, max(data3$nsize), by = 10))
+    fig = g+b+f+c+lab+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(expand = c(0, 0), breaks = seq(0, as.numeric(max(data3$size_cate)), by = 10))
     
     ggsave(file = paste0(dir_output, "/", sp[i], "length.png"), plot = fig, units = "in", width = 11.69, height = 8.27)
+    
+    
   }
   
   
