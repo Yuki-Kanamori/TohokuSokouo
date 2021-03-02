0d0bea1cc28cddb53552a28e22b7aca27974ea09
Yuki-Kanamori <y.kanamory@gmail.com>
2021-03-02 11:57:14 +0900

    not run with an error; $ operator is invalid for atomic vectors

--- length.R
+++ length.R
@@ -27,7 +27,7 @@ for(i in 1:length(sp)){
   l = max(l$nsize)
   
   temp2 = data.frame(年 = as.numeric(str_sub(temp$調査種類名称, 1, 4)), 和名 = paste0(sp[i]), 南北 = c("北部", "南部"), l = temp[, 16:(16+l)])
-  temp2 = temp2 %>% gather(key = size_cate, value = B, 4:(ncol(temp2))) %>% mutate(size_cate = str_sub(size_cate, 5, 6))
+  temp2 = temp2 %>% gather(key = size_cate, value = B, 4:(ncol(temp2))) %>% mutate(size_cate = str_sub(size_cate, 5, 6), B = B/1000)
   new = rbind(new, temp2)
 }
 summary(new)
@@ -41,16 +41,30 @@ levels(all$NS)
 summary(all)
 
 # function ------------------------------------------------------
-length = function(data){
+plot_length = function(data){
   sp = unique(data$sp)
   
   for(i in 1:length(sp)){
-    data = data %>% filter(sp == sp[i])
-    check = data %>% group_by(year) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
+    # data2 = data %>% dplyr::filter(sp == sp[i]) #なぜかうまく引っ掛からない
+    data2 = data[str_detect(data$sp, sp[i]), ]
+    check = data2 %>% group_by(year) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
+    remove = check$year
     
-    g = ggplot(data %>% filter(year != check$year), aes(x = as.numeric(size_cate), y = B/1000, fill = NS))
-    b = geom_bar(position = "dodge",stat = "identity",width = 1.5, colour = "black")
-    f = facet_wrap(~ year, ncol = 7)
+    check2 = data2 %>% group_by(size_cate) %>% summarize(sum = sum(B)) %>% filter(sum == 0)
+    remove2 = as.numeric(check2$size_cate)
+    remove2 = remove2[-1]
+    
+    summary(data2)
+    
+    if(nrow(check) > 0){
+      data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(year != remove, size_cate < min(remove2))
+    }else{
+      data3 = data2 %>% mutate(nsize = as.numeric(size_cate)) %>% filter(nsize < min(remove2))
+    }
+    
+    g = ggplot(data3, aes(x = nsize, y = B/1000, fill = NS))
+    b = geom_bar(position="dodge", stat = "identity", width = 0.8, colour = "black")
+    f = facet_wrap(~ year, ncol = 6)
     c = scale_fill_manual(values = c("black", NA))
     lab = labs(x = "体長（cm）", y = "尾数 (千尾)", colour = "エリア")
     
@@ -66,7 +80,7 @@ length = function(data){
                #legend.position = c(0.75, 0.05),
                legend.background = element_rect(fill = "white", size = 0.4, linetype = "solid", colour = "black"))
     
-    fig = g+b+f+c+lab+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(expand = c(0, 0), breaks = seq(0, max(as.numeric(data$size_cate)), by = 10))
+    fig = g+b+f+c+lab+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(expand = c(0, 0), breaks = seq(0, max(data3$nsize), by = 10))
     
     ggsave(file = paste0(dir_output, "/", sp[i], "length.png"), plot = fig, units = "in", width = 11.69, height = 8.27)
   }
@@ -77,5 +91,5 @@ length = function(data){
 
 
 # run -----------------------------------------------------------
-length(data = all)
+plot_length(data = all)
 
